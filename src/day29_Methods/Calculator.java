9bea7d1c8aecb0d9001e65fc19413ac76054fbb2
MVK-USA <mvk2015@yahoo.com>
2020-11-13 16:17:01 -0500

    calculator task

---
+++ src/day29_Methods/Calculator.java
@@ -0,0 +1,38 @@
+package day29_Methods;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        calculator( 10, '-', 50);
+    }
+
+
+    public static void calculator(double n1, char operator, double n2) {
+
+        boolean isValid = operator == '+' || operator == '-' || operator == '*'
+                || operator == '/' || operator == '%';
+        if (isValid) {
+            switch (operator) {
+                case '+':
+                    System.out.println("Addition: " + (n1 + n2));
+                    break;
+                case '-':
+                    System.out.println("Subtraction: " + (n1 - n2));
+                    break;
+                case '*':
+                    System.out.println("Multiplication: " + (n1 * n2));
+                    break;
+                case '/':
+                    System.out.println("Division: " + (n1 / n2));
+                    break;
+                default:
+                    System.out.println("Remainder: "+ (n1%n2));
+            }
+        } else {
+            System.out.println("Invalid Operator");
+        }
+    }
+
+}
+
