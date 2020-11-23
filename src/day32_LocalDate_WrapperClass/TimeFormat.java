6ec95f8343d9dd955fbff72737d525ae7255ff96
MVK-USA <mvk2015@yahoo.com>
2020-11-23 12:43:08 -0500

    date and time

--- src/day32_LocalDate_WrapperClass/TimeFormat.java
+++ src/day32_LocalDate_WrapperClass/TimeFormat.java
@@ -1,7 +1,10 @@
 package day32_LocalDate_WrapperClass;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import  java.time.format.DateTimeFormatter;
 import java.util.zip.DataFormatException;
 
@@ -15,6 +18,12 @@ public class TimeFormat {
     name of day: E(3 letters); EEEE (FULL NAME)
 
     Month/ Day /Year
+
+    LocalTime:
+    hours: hh
+    minutes: mm
+    seconds: ss
+    am/pm: a
      */
 
     public static void main(String[] args) {
@@ -24,6 +33,27 @@ public class TimeFormat {
         System.out.println(today);
 
         System.out.println(today.format(df));
+
+
+        DateTimeFormatter tf = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        LocalTime now = LocalTime.now();
+
+        System.out.println(now);
+
+        System.out.println(now.format(tf));
+
+        LocalTime t1 = LocalTime.of(23,35,55);
+        System.out.println(t1);
+        System.out.println(t1.format(tf));
+
+        //
+        // Monday 12:40 pm  Nov/23/2020
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE, hh:mm a, MMM/dd/yyyy");
+        LocalDateTime dt1 = LocalDateTime.of(2020,11,23,12,40);
+
+        System.out.println(dt1);
+        System.out.println(dtf.format(dt1));
+
 
     }
 }
