26399565f41848e92b0d2e643793b904f3db50a7
MVK-USA <mvk2015@yahoo.com>
2020-11-27 15:32:38 -0500

    remove letter & digits

--- src/Office_Hours/Practice_11_27_2020/RemoveSpecialCharacters.java
+++ src/Office_Hours/Practice_11_27_2020/RemoveSpecialCharacters.java
@@ -4,7 +4,15 @@ public class RemoveSpecialCharacters {
 
     public static void main(String[] args) {
 
-        String str = "ab!cd#ef%g$h^igkl*";
+        String str = "ab!cd#ef%g$h^igkl*123456";
 
+        String result =" ";
+
+        for(char each: str.toCharArray()){
+            if(Character.isLetterOrDigit(each)){
+                result += each;
+            }
+        }
+        System.out.println(result);
     }
 }
