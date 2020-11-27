e91359cdbdb58fd92f1f8c15c01a7ab26b7c1d7c
MVK-USA <mvk2015@yahoo.com>
2020-11-27 15:28:17 -0500

    find duplicate

---
+++ src/Office_Hours/Practice_11_27_2020/RemoveSpecialCharacters.java
@@ -0,0 +1,10 @@
+package Office_Hours.Practice_11_27_2020;
+
+public class RemoveSpecialCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "ab!cd#ef%g$h^igkl*";
+
+    }
+}
