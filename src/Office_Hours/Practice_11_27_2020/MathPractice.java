782175f0ad2f166d53ea2d50be9eb5b0d592b4f9
MVK-USA <mvk2015@yahoo.com>
2020-11-27 16:48:16 -0500

    mathematics

--- src/Office_Hours/Practice_11_27_2020/MathPractice.java
+++ src/Office_Hours/Practice_11_27_2020/MathPractice.java
@@ -1,5 +1,7 @@
 package Office_Hours.Practice_11_27_2020;
 
+import java.text.DecimalFormat;
+
 public class MathPractice {
 
     public static void main(String[] args) {
@@ -17,11 +19,11 @@ public class MathPractice {
         int n4 = Math.min(100, 200);
         System.out.println("min number: " + n4);
 
-        int[] numbers = {1,2,3,4,5,6,7,8,9};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int max = numbers[0];
         int min = numbers[0];
 
-        for ( int each: numbers){
+        for (int each : numbers) {
             max = Math.max(each, max);
             min = Math.min(each, min);
         }
@@ -35,9 +37,15 @@ public class MathPractice {
         double n5 = Math.sqrt(100.0);
         System.out.println(n5);
 
-        System.out.println(calculateAreaOfCircle(3));
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        double r1 = calculateAreaOfCircle(3);
+        System.out.println(r1);
+        System.out.println(df.format(r1));
+
     }
-    public static double calculateAreaOfCircle(double r){
-        return Math.PI*Math.pow(r,2);
+
+    public static double calculateAreaOfCircle(double r) {
+        return Math.PI * Math.pow(r, 2);
     }
 }
