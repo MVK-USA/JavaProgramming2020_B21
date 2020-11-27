9cf74a8f143ea0fa3b03a22c65f54d19b8b5efdf
MVK-USA <mvk2015@yahoo.com>
2020-11-27 13:59:15 -0500

    initial comments

---
+++ src/Office_Hours/Practice_11_27_2020/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package Office_Hours.Practice_11_27_2020;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,3,4};  // [1,2,3,4]
+
+        ArrayList<Integer> list = new ArrayList<>();  //[1, 2, 3, 4]
+
+        for(int each: arr){
+            if(!list.contains(each)){
+               list.add(each);
+            }
+
+        }
+        System.out.println(list);
+
+        int[] nonDup = new int[list.size()];
+
+        for(int i = 0; i<=nonDup.length-1; i++){
+            nonDup[i] = list.get(i);
+        }
+        System.out.println(Arrays.toString(nonDup));
+    }
+}
