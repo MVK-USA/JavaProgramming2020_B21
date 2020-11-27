15b30a71ced7c4b828215c1961181ec1af6fb89d
MVK-USA <mvk2015@yahoo.com>
2020-11-27 16:10:02 -0500

    initial comments

---
+++ src/Office_Hours/Practice_11_27_2020/Anagram.java
@@ -0,0 +1,12 @@
+package Office_Hours.Practice_11_27_2020;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        /*
+        str1 = Listen
+        str2 = Silent
+         */
+
+    }
+}
--- src/Office_Hours/Practice_11_27_2020/CharacterPractice.java
+++ src/Office_Hours/Practice_11_27_2020/CharacterPractice.java
@@ -4,12 +4,31 @@ public class CharacterPractice {
 
     public static void main(String[] args) {
 
+        int countSpecialChars = 0;
+        int countLetters = 0;
+        int countDigits = 0;
 
-                for(int i=1; i<= Character.MAX_VALUE; i++) {
-                    char each = (char) i;
-                    if (Character.isLetter(each)) {
-                        System.out.print(each + " ");
-                    }
-                }
+
+        for(int i = 1;  i<= Character.MAX_VALUE; i++){
+            char each = (char)i;
+            if(!Character.isLetterOrDigit(each)) {
+                // Character.isLetter(each) for printing all letters
+                countSpecialChars++;
+            }else if(Character.isLetter(each)){
+                countLetters++;
+            }else{
+                countDigits++;
+            }
+        }
+
+        System.out.println("countSpecialChars = " + countSpecialChars);
+        System.out.println("countLetters = " + countLetters);
+        System.out.println("countDigits = " + countDigits);
+
+        System.out.println(countSpecialChars+countLetters+countDigits);
+
+
+
     }
+
 }
