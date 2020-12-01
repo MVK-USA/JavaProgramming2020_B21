038c3095182bbcd128c7cb3585c67266be37a6fb
MVK-USA <mvk2015@yahoo.com>
2020-12-01 10:40:40 -0500

    remove: countries & dates

---
+++ src/day35_CustomClass/ArrayListPractice2.java
@@ -0,0 +1,16 @@
+package day35_CustomClass;
+
+public class ArrayListPractice2 {
+
+    public static void main(String[] args) {
+
+    }
+}
+/*
+ task03:    1. create an Array of String called jobTitles
+            2. write a program that only keeps the jobTitles that are: SDET and QA
+    task04:
+        Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
+                solution 1: use lambda
+                solution 2: do not use lambda
+ */
