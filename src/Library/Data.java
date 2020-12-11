2d1251133505878f8ec2a21cb94e9e2047ad9b37
MVK-USA <mvk2015@yahoo.com>
2020-12-11 10:22:51 -0500

    initial commit

---
+++ src/Library/Data.java
@@ -0,0 +1,17 @@
+package Library;
+
+public class Data {
+
+    public static String data1 = "A";
+    public static int data2 = 100;
+
+    public static void method1(){
+        System.out.println("Method 1");
+    }
+    public static void method2(){
+        System.out.println("Method 2");
+     }
+
+    public String dat3, data4, data5, data6;
+
+}
