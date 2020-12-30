0c85ffba3e9fa1b801a470113230c3976bd83384
MVK-USA <mvk2015@yahoo.com>
2020-12-29 20:23:56 -0500

    initial commit

---
+++ src/day44_Inheritance/ChromeDriver.java
@@ -0,0 +1,25 @@
+package day44_Inheritance;
+
+import day44_Inheritance.WebDriverTask.WebDriver;
+
+public class ChromeDriver extends WebDriver {
+
+    public ChromeDriver( ) {
+        super("Chrome Browser", "V84.0.1");
+    }
+
+    @Override
+    public void get(String URL) {
+        System.out.println("opening "+URL+" the chrome driver");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("closing chrome driver");
+    }
+
+    @Override
+    public void maximize() {
+        System.out.println("Maximizing chrome driver");
+    }
+}
