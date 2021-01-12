8218f894e2a4160dd716b02b97566d830fd5b938
MVK-USA <mvk2015@yahoo.com>
2021-01-12 16:13:57 -0500

    initial commit

---
+++ src/day47_Abstraction/EmployeeTask/Developer.java
@@ -0,0 +1,18 @@
+package day47_Abstraction.EmployeeTask;
+
+public class Developer extends Employee{
+
+    public Developer(String name, int age, char gender, String jobTitle, String ID, double salary) {
+        super(name, age, gender, jobTitle, ID, salary);
+    }
+
+    @Override
+    void work() {
+        System.out.println("Developer is writing codes");
+    }
+
+    @Override
+    protected void sleep() {
+        System.out.println("Developers usuallu sleep 6 hours");
+    }
+}
---
+++ src/day47_Abstraction/EmployeeTask/UberDriver.java
@@ -0,0 +1,17 @@
+package day47_Abstraction.EmployeeTask;
+
+public final class UberDriver extends Employee {
+    public UberDriver(String name, int age, char gender, String jobTitle, String ID, double salary) {
+        super(name, age, gender, jobTitle, ID, salary);
+    }
+
+    @Override
+    void work() {
+        System.out.println("Driving Uber");
+    }
+
+    @Override
+    protected void sleep() {
+        System.out.println("Uber drives usually sleep 9 hours");
+    }
+}
