f258c65fd959d398c0b133abbb4249ee33abf700
MVK-USA <mvk2015@yahoo.com>
2021-01-14 12:08:15 -0500

    initial commit

--- src/day48_Abstraction/AnimalTask/Shark.java
+++ src/day48_Abstraction/AnimalTask/Shark.java
@@ -1,4 +1,23 @@
 package day48_Abstraction.AnimalTask;
 
-public class Shark {
+public class Shark extends Animal implements Predator, Swimable{
+    public Shark(String name, String breed, char gender, int age) {
+        super(name, breed, gender, age);
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Sharks sleep 0 hours");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("Sharks hunting for the fish");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Sharks can swim 20 hours");
+
+    }
 }
